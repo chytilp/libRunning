6b0d9ae5e697cc2fa2b3a4032ad45bd59816558a
Patrik Chytil <pchytil@elektroline.cz>
2026-07-07 14:43:25 +0200

    add get_sections and get_aggregagtions lib functions

--- src/libRunning/__init__.py
+++ src/libRunning/__init__.py
@@ -1,5 +1,5 @@
 from libRunning.core.lib_funcs import (get_aggregation, get_aggregation_grades, get_compare, get_dashboard, get_date,
-                             get_dates, get_routes, get_section, get_section_grades)
+                             get_dates, get_routes, get_section, get_section_grades, get_sections, get_aggregations)
 from libRunning.model.printing import (CellIdentModel, CompareModel, DashboardModel, GradeModel, RouteModel, SectionModel,
                              SectionsModel, TrainingModel, TrainingsModel)
 
@@ -21,5 +21,7 @@ __all__ = [
     "get_dates",
     "get_routes",
     "get_section",
-    "get_section_grades"
+    "get_section_grades",
+    "get_sections",
+    "get_aggregations",
 ]
